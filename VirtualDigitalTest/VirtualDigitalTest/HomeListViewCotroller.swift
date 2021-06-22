1b68a56d0327809b507651f2b467d1449dbb09ee
luisarturogutierrez <l.gutierrez@itnnovation.com>
2021-06-22 12:33:08 -0500

    Add HomeListVC

--- VirtualDigitalTest/VirtualDigitalTest/HomeListViewCotroller.swift
+++ VirtualDigitalTest/VirtualDigitalTest/HomeListViewCotroller.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeListViewCotroller: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
